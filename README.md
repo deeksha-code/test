3bd335ca2c43f7ad8355e20fca110bc1e4394c2e
Deeksha Shetty <deekshashetty736@gmail.com>
2025-07-30 10:31:19 +0530

    accordion using vanilla Js

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This repository provides a basic structure for React and Node.js templates, incl
 1.Pagination implementation using static (constant) data.
 
 2.Pagination implementation with both frontend and backend integration,it includes both mongodb and mongoose Intergration
+
+3.Accordion implemenation using vanillaJS and reactJS
--- frontend/src/components/Accordion.jsx
+++ frontend/src/components/Accordion.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 function Accordion() {
-  const [isOpen,setIsOpen]=useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [index, setIndex] = useState(0);
+  const handleClick = (i) => {
+    if (index === i) { //If we click on the same Item again
+      setIsOpen((open) => !open);
+    } else {//If we click on other items
+      setIsOpen(true);
+      setIndex(i);
+    }
+  };
   return (
     <div
       style={{
@@ -16,29 +25,57 @@ function Accordion() {
         <>
           {/* <h3>Content1 &#9654; &#9660;</h3> */}
           {/* <h3>Content1 {isOpen ? '▼' : '▶'}</h3> */}
-          <h3>Content1 <span onClick={()=>{setIsOpen((open)=>!open)}}>{isOpen?String.fromCharCode(9660) : String.fromCharCode(9654)}</span></h3>
-          {isOpen?<>Hello of content1</>:null}
+          <h3>
+            Content1{" "}
+            <span style={{cursor:"pointer"}} onClick={() => handleClick(1)}>
+              {isOpen && index === 1
+                ? String.fromCharCode(9660)
+                : String.fromCharCode(9654)}
+            </span>
+          </h3>
+          {isOpen && index === 1 ? <>Hello of content1</> : null}
         </>
       </div>
 
       <div>
         <>
-         <h3>Content2 <span onClick={()=>{setIsOpen((open)=>!open)}}>{isOpen?String.fromCharCode(9660) : String.fromCharCode(9654)}</span></h3>
-          {isOpen?<>Hello of content2</>:null}
+          <h3>
+            Content2{" "}
+            <span style={{cursor:"pointer"}} onClick={() => handleClick(2)}>
+              {isOpen && index === 2
+                ? String.fromCharCode(9660)
+                : String.fromCharCode(9654)}
+            </span>
+          </h3>
+          {isOpen && index == 2 ? <>Hello of content2</> : null}
         </>
       </div>
 
       <div>
         <>
-                   <h3>Content3 <span onClick={()=>{setIsOpen((open)=>!open)}}>{isOpen?String.fromCharCode(9660) : String.fromCharCode(9654)}</span></h3>
-          {isOpen?<>Hello of content3</>:null}
+          <h3>
+            Content3{" "}
+            <span style={{cursor:"pointer"}} onClick={() => handleClick(3)}>
+              {isOpen && index === 3
+                ? String.fromCharCode(9660)
+                : String.fromCharCode(9654)}
+            </span>
+          </h3>
+          {isOpen && index === 3 ? <>Hello of content3</> : null}
         </>
       </div>
 
       <div>
         <>
-                  <h3>Content4 <span onClick={()=>{setIsOpen((open)=>!open)}}>{isOpen?String.fromCharCode(9660) : String.fromCharCode(9654)}</span></h3>
-          {isOpen?<>Hello of content4</>:null}
+          <h3>
+            Content4{" "}
+            <span style={{cursor:"pointer"}} onClick={() => handleClick(4)}>
+              {isOpen && index === 4
+                ? String.fromCharCode(9660)
+                : String.fromCharCode(9654)}
+            </span>
+          </h3>
+          {isOpen && index === 4 ? <>Hello of content4</> : null}
         </>
       </div>
     </div>
